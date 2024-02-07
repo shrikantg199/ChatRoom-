0350e948c576d4729ffcde7427f522c1bfb6d97d
shrikant gaikwad <shrikantg199@gmail.com>
2024-02-07 18:50:48 +0530

    initial commit

--- src/Firebase.jsx
+++ src/Firebase.jsx
@@ -3,8 +3,8 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.API_KEY,
-  authDomain: import.meta.AUTH_DOMAIN,
+  apiKey: "AIzaSyCrRiz8fSkusivO2c1_R8VCplcYfjTGilw",
+  authDomain: "chatweb-29f12.firebaseapp.com",
   projectId: "chatweb-29f12",
   storageBucket: "chatweb-29f12.appspot.com",
   messagingSenderId: "1083036935901",
